199b4f24fc2de81d03ce91b3a2d415b3f0288a57
Michael P Camilleri <michael.p.camilleri@ed.ac.uk>
2022-04-04 18:41:30 +0100

    Updated Version for packaging

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     license="GNU GPL",
     keywords=["extensions", "parallel", "utilities"],
     url="https://github.com/michael-camilleri/mpctools",
-    download_url="https://github.com/michael-camilleri/mpctools/archive/v_0600.tar.gz",
+    download_url="https://github.com/michael-camilleri/mpctools/archive/v_0700.tar.gz",
     long_description=long_description,
     long_description_content_type="text/markdown",
     data_files=[("", ["LICENSE"])],
